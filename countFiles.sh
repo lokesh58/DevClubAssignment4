24557eb61784c1e801d4b9eb555c5964ad1927c9
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-13 22:08:06 +0530

    Removed unnecessaryif statement

--- countFiles.sh
+++ countFiles.sh
@@ -8,10 +8,10 @@ if [ $# -eq 0 ]; then
 	exit $exit_code
 else
 	dir=$1;
-	if [ ! -d "$dir" ]; then
+	if [ ! -r "$dir" ]; then
 		exit $exit_code
-	elif [ ! -r "$dir" ]; then
-		exit $exit_code
+	#elif [ ! -r "$dir" ]; then
+	#	exit $exit_code
 	fi
 
 	if [ $# -gt 1 ]; then
