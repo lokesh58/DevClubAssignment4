6eaa0feed57b75c88d0c7d8fec0bb6fddc5d953f
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-13 21:17:45 +0530

    Added the basic functionality

---
+++ uname.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+declare -r exit_code=1
+
+file=$1
+user=$2
+OIFS="$IFS"
+
+if [ $# -lt 2 ]; then
+	# echo "Insufficient Arguments"
+	exit $exit_code
+fi
+
+while read line; do
+	# echo $line
+	if [[ "$line" != *":x:"*":"*":"*":"*":"* ]]; then
+		# echo "Incorrect format"
+		exit $exit_code
+	fi
+	
+	IFS=":"
+
+	read -r -a attr <<< "$line"
+	if [ "${attr[0]}" == "$user" ]; then
+		echo "${attr[4]}"
+		exit 0
+	fi
+
+	IFS=$OIFS
+done < "$file"
+
+echo "Invalid Username"
