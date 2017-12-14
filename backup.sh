2b91c4664e928a6283dbc1c09f503b6b77dba019
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-14 18:04:16 +0530

    Added the display of files copied

--- backup.sh
+++ backup.sh
@@ -37,12 +37,25 @@ cpdir1todir2() {
 			if [ ! -d "$2/$file" ]; then
 				mkdir $2/$file
 			fi
-			$(cpdir1todir2 $1/$file $2/$file)
+			if [ $# -eq 2 ]; then
+				arg3=$file
+			else
+				arg3=$3/$file
+			fi
+			cpdir1todir2 $1/$file $2/$file $arg3
 		elif [ ! -f "$2/$file" ]; then
 			cp $1/$file $2
+			if [ $# -eq 2 ]; then
+				echo "$file"
+			else
+				echo "$3/$file"
+			fi
 		fi
 	done
 }
 
+echo "Files copied from $1 to $2 are:"
 cpdir1todir2 $dir1 $dir2
+
+echo "Files copied from $2 to $1 are:"
 cpdir1todir2 $dir2 $dir1
