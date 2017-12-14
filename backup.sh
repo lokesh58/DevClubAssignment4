609f5251454442970d52230d81049a3ef0b238df
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-14 15:12:02 +0530

    Files not in subfolder are synced

--- backup.sh
+++ backup.sh
@@ -6,26 +6,51 @@ if [ $# -lt 2 ]; then
 	exit $exit_code
 fi
 
+dir0=$PWD
 dir1=$1
 dir2=$2
 
 if [ ! -d "$dir1" -o ! -r "$dir1" ]; then
 	# echo "$dir1 not valid"
 	exit $exit_code
+else
+	cd $dir1
+	dir1=$PWD
+	# echo "$dir1"
+	cd $dir0
 fi
 
 if [ ! -d "$dir2" -o ! -r "$dir2" ]; then
 	# echo "$dir2 not valid"
 	exit $exit_code
+else
+	cd $dir2
+	dir2=$PWD
+	# echo "$dir2"
+	cd $dir0
 fi
 
 declare -a dir1todir2
 declare -a dir2todir1
 
-for file in $dir1/*; do
-	echo "$file"
+cd $dir1
+for file in *; do
+	# echo "$file"
+	if [ -d "$file" ]; then
+		echo "Folder $file not synced"
+	elif [ ! -f "${dir2}/$file" ]; then 
+		cp ${dir1}/$file ${dir2}
+	fi
 done
+cd $dir0
 
-for file in $dir2/*; do
-	echo "$file"
+cd $dir2
+for file in *; do
+	# echo "$file"
+	if [ -d "$file" ]; then
+		echo "Folder $file not synced"
+	elif [ ! -f "${dir1}/$file" ]; then
+		cp ${dir2}/$file ${dir1}
+	fi
 done
+cd $dir0
