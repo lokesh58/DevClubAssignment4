098e4882f6d9587c610f27c9edb1846413b6f708
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-14 17:51:33 +0530

    Changed the way files are copied, created a function

--- backup.sh
+++ backup.sh
@@ -9,12 +9,6 @@ fi
 dir0=$PWD
 dir1=$1
 dir2=$2
-display=0
-
-if [ $# -eq 4 ]; then
-	display=$3
-	app=$4
-fi
 
 if [ ! -d "$dir1" -o ! -r "$dir1" ]; then
 	# echo "$dir1 not valid"
@@ -36,73 +30,19 @@ else
 	cd $dir0
 fi
 
-cd $dir1
-
-if [ $display -eq 0 ]; then
-	echo "Files copied from $1 to $2 are:"
+cpdir1todir2() {
+	cd $1
 	for file in *; do
-		if [ ! -d "$file" ]; then
-			if [ ! -e "${dir2}/$file" ]; then
-				echo "$file"
+		if [ -d "$file" ]; then
+			if [ ! -d "$2/$file" ]; then
+				mkdir $2/$file
 			fi
+			$(cpdir1todir2 $1/$file $2/$file)
+		elif [ ! -f "$2/$file" ]; then
+			cp $1/$file $2
 		fi
 	done
-fi
+}
 
-for file in *; do
-	# echo "$file"
-	if [ -d "$file" ]; then
-		if [ ! -d "${dir2}/$file" ]; then
-			mkdir ${dir2}/$file
-		fi
-		cd $dir0
-		if [ $display -eq 0 ]; then
-			arg4=$file
-		else
-			arg4=$app/$file
-		fi
-		$0 ${dir1}/$file ${dir2}/$file 1 $arg4
-		cd $dir1
-	elif [ ! -f "${dir2}/$file" ]; then 
-		cp ${dir1}/$file ${dir2}
-		if [ $display -eq 1 ]; then
-			echo "$app/$file"
-		fi
-	fi
-done
-
-cd $dir2
-
-if [ $display -eq 0 ]; then
-	echo "Files copied from $2 to $1 are:"
-	for file in *; do
-		if [ ! -d "$file" ]; then
-			if [ ! -e "${dir1}/$file" ]; then
-				echo "$file"
-			fi
-		fi
-	done
-fi
-
-for file in *; do
-	# echo "$file"
-	if [ -d "$file" ]; then
-		if [ ! -d "${dir1}/$file" ]; then
-			mkdir ${dir1}/$file
-		fi
-		cd $dir0
-		if [ $display -eq 0 ]; then
-			arg4=$file
-		else
-			arg4=$app/$file
-		fi
-		$0 ${dir2}/$file ${dir1}/$file 2 $arg4
-		cd $dir2
-	elif [ ! -f "${dir1}/$file" ]; then
-		cp ${dir2}/$file ${dir1}
-		if [ $display -eq 2 ]; then
-			echo "$app/$file"
-		fi
-	fi
-done
-cd $dir0
+cpdir1todir2 $dir1 $dir2
+cpdir1todir2 $dir2 $dir1
